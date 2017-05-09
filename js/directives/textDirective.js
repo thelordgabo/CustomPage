9955493550259e27f2d7822cf94aed116d1ce5c1
Gabriel Anzaldo <anzaldo.alvarado.gabriel@gmail.com>
2017-05-09 09:57:47 -0300

    Broken reference fixed, make small refactor over textDirective.

--- js/directives/textDirective.js
+++ js/directives/textDirective.js
@@ -1,20 +1,25 @@
-var app = angular.module('customPage');
+(function () {
+  'use strict';
+  var app = angular.module('customPage');
 
-app.directive('textDirective', function(textService){
-  return {
-    restrict: "E",
-    templateUrl: 'js/templates/textDirective.html',
-    controller : function ($scope,$timeout){
-      setTimeout(function () {
-        jQuery('.post').addClass("hidden-div").viewportChecker({
-          classToAdd: 'visible-div animated fadeIn'
-        });
-      }, 10);
-      $scope.textList = {};
-      var promise = textService.getText();
-      promise.then(function(data) {
+  app.directive('textDirective', textDirective);
+
+  function textDirective(textService) {
+    return {
+      restrict: "E",
+      templateUrl: 'js/templates/textDirective.html',
+      controller: function ($scope, $timeout) {
+        setTimeout(function () {
+          jQuery('.post').addClass("hidden-div").viewportChecker({
+            classToAdd: 'visible-div animated fadeIn'
+          });
+        }, 10);
+        $scope.textList = {};
+        var promise = textService.getText();
+        promise.then(function (data) {
           $scope.textList = data.data;
-      });
+        });
+      }
     }
   }
-});
+})();
