51cee9dda5cde69a6eea2f30845ce6ea2fa23165
Gabriel Anzaldo <anzaldo.alvarado.gabriel@gmail.com>
2017-05-09 10:19:00 -0300

    Refactoring of textDirective completed.

--- js/directives/textDirective.js
+++ js/directives/textDirective.js
@@ -5,21 +5,28 @@
   app.directive('textDirective', textDirective);
 
   function textDirective(textService) {
-    return {
+    var directive = {
       restrict: "E",
       templateUrl: 'js/templates/textDirective.html',
-      controller: function ($scope, $timeout) {
-        setTimeout(function () {
-          jQuery('.post').addClass("hidden-div").viewportChecker({
-            classToAdd: 'visible-div animated fadeIn'
-          });
-        }, 10);
-        $scope.textList = {};
-        var promise = textService.getText();
-        promise.then(function (data) {
-          $scope.textList = data.data;
+      controller: testDirectiveController,
+      controllerAs: 'vm',
+      bindToController: true
+    };
+
+    return directive;
+
+    function testDirectiveController($timeout) {
+      var vm = this;
+      setTimeout(function () {
+        jQuery('.post').addClass("hidden-div").viewportChecker({
+          classToAdd: 'visible-div animated fadeIn'
         });
-      }
+      }, 10);
+      vm.textList = {};
+      var promise = textService.getText();
+      promise.then(function (data) {
+        vm.textList = data.data;
+      });
     }
   }
 })();
